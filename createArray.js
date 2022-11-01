36395be13645104696e74a3e60d558a803f56866
HugoSanche <hbmclaren@gmail.com>
2022-10-31 23:07:59 -0600

    Adding file createArray.js.- Its an example how create an array in javascript

---
+++ createArray.js
@@ -0,0 +1,38 @@
+class myArray{
+    constructor(){
+      this.lenght=0;
+      this.data={};
+    }
+get(index){
+  return this.data[index];
+}
+push(item){
+  this.data[this.lenght]=item;
+  this.lenght++;
+  return this.lenght;
+}
+
+pop(){
+  const lastItem=this.data[this.lenght-1];
+  delete this.data[this.lenght-1];
+  this.lenght--;
+  return lastItem;
+}
+//[5,8,5,3]
+shiftItems(index){
+    for (let i=index; i<this.data.lenght-1; i++)
+    {
+      this.data[i]=this.data[i+1];
+    }
+      delete this.data[this.lenght-1];
+      this.lenght--;
+}
+}
+
+const newArray= new myArray();
+newArray.push("Hi");
+newArray.push("Hugo");
+newArray.push("!");
+newArray.pop();
+newArray.shiftItems(1);
+console.log(newArray);
